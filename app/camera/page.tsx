79782039ee570e5b014e58a4618dfdde3d466d88
marcello120 <sarosi.marcell@gmail.com>
2026-09-10 00:54:01 +0200

    add link

--- app/camera/page.tsx
+++ app/camera/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
 
 export default function CameraPage() {
-  redirect("https://example.com");
+  redirect("https://pov.camera/qr/44C7A43D-0FF3-401E-8248-BA30C607CD1D");
 }
