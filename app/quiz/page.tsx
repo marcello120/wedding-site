6b0dba4d5678c28921d65d7e3ef4dc51520cb277
marcello120 <sarosi.marcell@gmail.com>
2026-09-18 16:33:13 +0200

    Change redirect URL in CameraPage component

--- app/quiz/page.tsx
+++ app/quiz/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
 
 export default function CameraPage() {
-  redirect("https://example.com");
+  redirect("https://kahoot.it/?pin=555045&refer_method=link");
 }
